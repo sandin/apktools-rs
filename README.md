730f7ac7eefcea5fa673856826bd2b0a0dc6476d
lds <lds2012@gmail.com>
2021-07-01 20:09:10 +0800

    add debuggable cmd

--- README.md
+++ README.md
@@ -4,9 +4,16 @@ Tools for android apk file.
 
 # Usage
 
+Get package name:
 ```
 $ apktools packagename <apkfile>
 $ output: com.your.package.name
+```
+
+Set `debuggable=true`:
+```
+$ apktools debuggable <apkfile>
+$ output: success
 ```
 
 # Build
--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ fn main() {
                     .about("get package name of apk")
                     .arg(Arg::with_name("apkfile").required(true).help("apk file"))
                 )
+        .subcommand(SubCommand::with_name("debuggable")
+                    .about("set debuggable=true of apk")
+                    .arg(Arg::with_name("apkfile").required(true).help("apk file"))
+            )
         .get_matches();
 
     if let Some(opts) = matches.subcommand_matches("packagename") {
@@ -27,6 +31,7 @@ fn main() {
 
         let zipfile = std::fs::File::open(&apkfile).unwrap();
         let mut archive = zip::ZipArchive::new(zipfile).unwrap();
+
         let mut manifest_file = match archive.by_name("AndroidManifest.xml") {
             Ok(file) => file,
             Err(..) => {
@@ -43,5 +48,29 @@ fn main() {
             strings_pool: Vec::new() 
         };
         read_chunk(&mut context, &mut cursor);
+    } else if let Some(opts) = matches.subcommand_matches("debuggable") {
+        let apkfile = opts.value_of("apkfile").expect("missing apkfile arg.");
+        let apkfile = std::path::Path::new(apkfile);
+        if !apkfile.exists() {
+            eprintln!("{} file is not exists!", apkfile.display());
+            process::exit(-1); 
+        }
+
+        let zipfile = std::fs::File::open(&apkfile).unwrap();
+        let mut archive = zip::ZipArchive::new(zipfile).unwrap();
+
+        let mut output_path = std::path::PathBuf::new();
+        output_path.push(apkfile);
+        output_path.set_extension("zip"); // TODO: rename a.apk -> a_debug.apk
+    
+        let output_file = std::fs::File::create(&output_path).unwrap();
+        let mut output_zip = zip::ZipWriter::new(output_file);
+    
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+            output_zip.raw_copy_file(file).unwrap();
+            // TODO: change manifest
+        }
+        output_zip.finish().unwrap();
     }
 }
